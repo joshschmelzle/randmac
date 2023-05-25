be5d5f39b143fc3776670983e18ebc09efae45a3
Josh Schmelzle <josh@joshschmelzle.com>
2023-05-25 16:44:49 -0400

    Bump version 0.1.0 -> 0.2.0

--- randmac/__version__.py
+++ randmac/__version__.py
@@ -8,7 +8,7 @@
 __title__ = "randmac"
 __description__ = "a random 12-digit mac address generator"
 __url__ = "https://github.com/joshschmelzle/randmac"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Josh Schmelzle"
 __author_email__ = "josh@joshschmelzle.com"
 __license__ = "BSD-3-Clause"
